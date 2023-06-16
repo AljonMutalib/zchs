5d57bab82ee072f4bbbdc2e451bc9d9dbf2ac20d
Aljon Mutalib <66012111+AljonMutalib@users.noreply.github.com>
2023-06-16 09:43:17 +0800

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,7 +1,3 @@
-// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,20 +7,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: MyHomePage(),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget{
+class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Aljon Mutalib'))
+      appBar: AppBar(title: Text('DICT')),
+      body: Text('Department of Information and Communications Technology')
     );
   }
 }
+
