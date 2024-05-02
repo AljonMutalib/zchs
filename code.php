90236a0809603e0883fd7184cdbe3d847dfdbcb0
Aljon Mutalib <66012111+AljonMutalib@users.noreply.github.com>
2024-05-02 14:12:02 +0800

    Update code.php

--- code.php
+++ code.php
@@ -1,85 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+function login(){
+    var username = $('#usernameForm');
+    var password = $('#passwordForm');
 
-<head>
+    var login_result = $('#login_result');
+    login_result.html('<div class="alert alert-success">Loading... Please wait. <i class="fa fa-spinner fa-spin" style="font-size:24px"></i></div>');
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Profiling System</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <script src="js/function.js"></script>
-</head>
-
-<body class="bg-gradient-primary">
-
-    <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <div id="login_result"></div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user"
-                                                id="usernameForm" aria-describedby="emailHelp"
-                                                placeholder="Enter username">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="passwordForm" placeholder="Enter Password">
-                                        </div>
-                                        
-                                        <a href="#" onclick="login()" class="btn btn-primary btn-user btn-block">
-                                            Login
-                                        </a>
-                                        <hr>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-
-    </div>
-
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
-
-</body>
-
-</html>
+    if (username.val() == '') {
+        username.focus();
+        login_result.html('<div class="alert alert-info">Please Enter the Username!!</div>');
+        return false;
+    }else if (password.val() == '') {
+        password.focus();
+        login_result.html('<div class="alert alert-info">Please Enter the Password!!</div>');
+        return false;
+    }else if (username.val() != '' && password.val() != '') {
+        var UrlToPass = "action=login&username=" + username.val() + "&password=" + password.val();
+        $.ajax({
+            type: 'POST',
+            data: UrlToPass,
+            url: 'config/checker.php',
+            success: function (returnval) {
+                switch (returnval) {
+                    case '0':
+                        login_result.html('<div class="alert alert-danger">Please Check Your Username and Password!!</div>');
+                        break;
+                    case '1':
+                        login_result.html('<div class="alert alert-success">Welcome!</div>');
+                        //window.location.href = 'pages/dashboard.php';
+                        break;
+                    default:
+                        console.log('Problem with sql query: ' + returnval);
+                        break;
+                }
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('AJAX Error: ' + errorThrown);
+            }
+        });
+    }
+    return false;
+}
